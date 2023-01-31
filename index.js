48ac7f9659f4fcfb46568dcd30908abb7f8636ca
Aymen FATHALLAH <aymenfathallah97@gmail.com>
2023-01-31 12:31:11 +0100

    list backet

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-import { ListBucketsCommand } from "@aws-sdk/client-s3";
+import { ListBucketsCommand, ListObjectsV2Command, DeleteObjectsCommand, DeleteBucketCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "./libs/sampleClient.js";
 
+export const run = async () => {
+  try {
+    const data = await s3Client.send(new ListBucketsCommand({}));
+    data.Buckets.forEach(
+      (element) => console.log(element.CreationDate.toUTCString() + " " + element.Name)
+    );
 
-export const run = async () => { 
-    try {
-        const data = await s3Client.send(new ListBucketsCommand({}));
-        data.Buckets.forEach(  
-            element => console.log( element.CreationDate.toUTCString() + " " + element.Name ) 
+    const bucketName = "datascientest-aymen-sdkdemo";
+    const deleteObjectsParams = { Bucket: bucketName };
+
+    await s3Client.send(
+      new ListObjectsV2Command(deleteObjectsParams),
+      (err, data) => {
+        if (err) throw err;
+
+        const objectsToDelete = data.Contents.map(({ Key }) => ({ Key }));
+
+        s3Client.send(
+          new DeleteObjectsCommand({ ...deleteObjectsParams, Delete: { Objects: objectsToDelete } }),
+          (err, data) => {
+            if (err) throw err;
+
+            s3Client.send(new DeleteBucketCommand({ Bucket: bucketName }), (err, data) => {
+              if (err) throw err;
+
+              console.log(`Successfully deleted bucket ${bucketName} and its contents.`);
+            });
+          }
         );
-        return data;
-    } catch (err) {
-        console.log("Error", err);
-    }
- };
+      }
+    );
 
- run();
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+run();
